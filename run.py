a40f6615deed5a0b55e13ae7a51d543f76073522
Khalem Connaughton <khalem.connaughton@gmail.com>
2019-01-23 17:41:52 +0000

    Added view-recipe.html - recipe being displayed

--- run.py
+++ run.py
@@ -10,6 +10,14 @@ app.config["MONGO_URI"] = "mongodb://admin:cod4cmtmazza@ds161894.mlab.com:61894/
 mongo = PyMongo(app)
 
 
+def check_for_input(value):
+    # If there was no input, return all results within that key by checking it exists.
+    if value == False or value == "":
+        return {"$exists": True}
+    # If there was input, just return input.
+    else:
+        return value
+
 @app.route("/")
 @app.route("/<new_recipes>")
 def index(new_recipes=None):
@@ -60,6 +68,12 @@ def filter_home():
         To filter through results, I will first declare that not all fields are required. I will then pass the input to the check_for_input() function
         There, it will find out if anything was filled in the fields. If the fields weren't filled in, I will just return all results within that key by checking it exists.
     """
+    
+    # Check if user is logged in
+    logged_in = False
+    if "username" in session:
+        logged_in = True
+    
     recipe_name = check_for_input(request.form["recipe_name"])
     country_of_origin = check_for_input(request.form.get("country_of_origin", False))
     category = check_for_input(request.form.get("category", False))
@@ -70,16 +84,27 @@ def filter_home():
     recipes = mongo.db.recipes.find({"recipe_name": recipe_name, "country_of_origin": country_of_origin, "category": category, "allergens": allergens, "tags": tags})
     # Get the amount of results
     count = recipes.count()
-    return render_template("index.html", recipes = recipes, logged_in = True, count = count) 
+    return render_template("index.html", recipes = recipes, logged_in = logged_in, count = count) 
 
+@app.route("/view_recipe/<recipe_id>")
+def view_recipe(recipe_id):
+    """
+        This function will pass through the selected recipe. It will check if the user is logged in, icrease the views of the recipe and display the recipe.
+    """
+    
+    # Check if user is logged in - must pass through so the option to add to cook book will be hidden from users who aren't logged in
+    logged_in = False
+    if "username" in session:
+        logged_in = True
+        
+    # The following lines of code will increase the views of the recipe and query the database. 
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    new_views = recipe["views"]
+    new_views = new_views + 1
+    mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, {"$set": {"views": new_views}})
+    
+    return render_template("view-recipe.html", recipe = recipe, logged_in = logged_in)
 
-def check_for_input(value):
-    # If there was no input, return all results within that key by checking it exists.
-    if value == False or value == "":
-        return {"$exists": True}
-    # If there was input, just return input.
-    else:
-        return value
 
 if __name__ == "__main__":
     app.secret_key = "$my$secret$key"
